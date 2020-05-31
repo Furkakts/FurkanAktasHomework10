0773f05a7a18a523981d43844faa05cdc8e7bfae
Furkakts <furkakts_4126@hotmail.com>
2020-05-31 21:29:19 +0300

    Update Fractional.java file
    I put the equals method into the file.

--- Fractional.java
+++ Fractional.java
@@ -167,6 +167,15 @@ public class Fractional {
 	}
 	
 	@Override
+	public boolean equals(Object obj) {
+		Fractional ratNumber = (Fractional)obj;
+		
+		if (ratNumber.numerator == ratNumber.denominator){
+			return true;
+		}else { return false; }
+	} 
+	
+	@Override
 	public String toString() {
 		if (isNaN()) {
 			return NotANumber;
